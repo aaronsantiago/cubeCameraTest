5488cc31b054e0aa311120db852e0084955fc825
Aaron Santiago <aaronsantiago+github@gmail.com>
2021-10-26 03:35:44 -0400

    setup basic cubecamera test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
+import { Canvas } from "@react-three/fiber";
+import {CubeCamera, OrbitControls} from "@react-three/drei";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <OrbitControls/>
+        <ambientLight/>
+        <directionalLight/>
+        <mesh scale={[1,1,1]} position={[0,-1.4,2]} rotation={[0,Math.PI/4,0]}>
+          <boxGeometry/>
+          <meshStandardMaterial></meshStandardMaterial>
+        </mesh>
+
+        <CubeCamera>
+          {(texture) => <mesh>
+            <sphereGeometry/>
+            <meshStandardMaterial metalness={1} envMap={texture}/>
+          </mesh>}
+        </CubeCamera>
+      </Canvas>
     </div>
   );
 }
